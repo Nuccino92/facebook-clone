0a6061cf762aaa94db11eaaa15faf1906c43fccb
Nuccino92 <anthnucci@hotmail.com>
2022-01-27 20:18:53 -0500

    login form/ conditional render dependant on location

---
+++ client/src/components/LogIn/LogInForm.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+
+const LogInForm = () => {
+  const location = useLocation();
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("clicked");
+  };
+
+  return location.pathname === "/login" ? (
+    <div className="LogInForm-container">
+      <h1 className="LogIn-form-header">facebook</h1>
+
+      <form className="LogIn-form" onSubmit={handleSubmit}>
+        <div className="LogIn-form-header">Log in to Facebook</div>
+        <input type="text" name="email" placeholder="Email adress"></input>
+        <input type="password" name="password" placeholder="Password"></input>
+        <button className="LogIn-submit-btn">Log In</button>
+        <div className="guest-account-link">Use Guest Account</div>
+        <div className="LogIn-form-break"></div>
+        <button className="LogIn-register-btn">Create New Account</button>
+      </form>
+    </div>
+  ) : (
+    <form className="LogIn-form" onSubmit={handleSubmit}>
+      <input type="text" name="email" placeholder="Email adress"></input>
+      <input type="password" name="password" placeholder="Password"></input>
+      <button className="LogIn-submit-btn">Log In</button>
+      <div className="guest-account-link">Use Guest Account</div>
+      <div className="LogIn-form-break"></div>
+      <button className="LogIn-register-btn">Create New Account</button>
+    </form>
+  );
+};
+
+export default LogInForm;
