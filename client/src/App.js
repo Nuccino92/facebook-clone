12bb0364a0f137e8fac61ef2c33b5166d6d8f4cb
Nuccino92 <anthnucci@hotmail.com>
2022-01-27 20:11:57 -0500

    added router, checks if user logged in

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LogIn from "./components/LogIn/LogIn";
+import LogInForm from "./components/LogIn/LogInForm";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [user] = useState(false);
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={user ? null : <LogIn />} />
+          <Route path="/login" element={<LogInForm />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
