66f55854e611658fda50145a9bed7d6d2572534a
Nuccino92 <anthnucci@hotmail.com>
2022-01-29 23:57:56 -0500

    homepage component

---
+++ client/src/components/Homepage/Homepage.js
@@ -0,0 +1,13 @@
+import Nav from "../Nav/Nav";
+import "./Homepage.css";
+
+const Homepage = () => {
+  return (
+    <div className="Homepage">
+      <Nav />
+      <div></div>
+    </div>
+  );
+};
+
+export default Homepage;
