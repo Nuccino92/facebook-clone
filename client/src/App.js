2547044bd66eae5efe1cb3f3a8d49e4eb0b4adf9
Nuccino92 <anthnucci@hotmail.com>
2022-01-29 23:57:44 -0500

    homepagwe component

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,15 @@ import LogIn from "./components/LogIn/LogIn";
 import LogInForm from "./components/LogIn/LogInForm";
 import { useState } from "react";
 import Register from "./components/Register/Register";
+import Homepage from "./components/Homepage/Homepage";
 
 function App() {
-  const [user] = useState(false);
+  const [user] = useState(true);
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={user ? null : <LogIn />} />
+          <Route path="/" element={user ? <Homepage /> : <LogIn />} />
           <Route path="/login" element={<LogInForm />} />
           <Route path="/register" element={<Register />} />
         </Routes>
