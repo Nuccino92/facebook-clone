e65a86102702de0be53adc072e08f98bdb71f8e5
Nuccino92 <anthnucci@hotmail.com>
2022-01-30 20:45:47 -0500

    completed navbar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -3,19 +3,32 @@ import facebookIcon from "./facebook-icon.png";
 import { AiOutlineSearch, AiFillCaretDown } from "react-icons/ai";
 import { IoMdHome } from "react-icons/io";
 import { BsPersonSquare } from "react-icons/bs";
+import { FaUserFriends } from "react-icons/fa";
+import { GoSignOut } from "react-icons/go";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   const [userData] = useState({
     firstname: "Anthony",
+    lastname: "Nucci",
     picture:
       "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg",
   });
 
+  const [profileMenu, setProfileMenu] = useState(false);
+
+  const handleMenu = () => {
+    setProfileMenu(!profileMenu);
+  };
+
   return (
     <div className="Nav">
       <div className="nav-left">
-        <img src={facebookIcon} alt="Facebook icon homepage button"></img>
+        <Link to="/">
+          <img src={facebookIcon} alt="Facebook icon homepage button"></img>
+        </Link>
+
         <div className="searchbar-container">
           <AiOutlineSearch />
           <input placeholder="Search Facebook" type="text"></input>
@@ -34,9 +47,41 @@ const Nav = () => {
           <img src={userData.picture} alt="stop"></img>
           <div>{userData.firstname}</div>
         </div>{" "}
-        <div className="down-arrow-container">
+        <div
+          onClick={handleMenu}
+          id="down-arrow"
+          className="down-arrow-container"
+        >
           <AiFillCaretDown />
         </div>
+        {profileMenu && (
+          <div className="profile-menu">
+            <div className="profile-menu-inner">
+              <ul>
+                <li className="profile-menu-first">
+                  <img src={userData.picture} alt="Profile"></img>
+                  <div>
+                    {userData.firstname} {userData.lastname}
+                    <p>See your profile</p>
+                  </div>
+                </li>
+                <div></div>
+                <li className="profile-menu-second">
+                  <div>
+                    <FaUserFriends />
+                  </div>
+                  <p>View Friend Requests</p>
+                </li>
+                <li className="profile-menu-third">
+                  <div>
+                    <GoSignOut />
+                  </div>
+                  <p>Log Out</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
