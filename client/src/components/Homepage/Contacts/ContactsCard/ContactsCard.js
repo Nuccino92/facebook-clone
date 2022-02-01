407925bad6cf15482649794dcdfec54a3b00a1fd
Nuccino92 <anthnucci@hotmail.com>
2022-01-31 22:51:14 -0500

    component for hover effect on contacts list

---
+++ client/src/components/Homepage/Contacts/ContactsCard/ContactsCard.js
@@ -0,0 +1,23 @@
+import "./ContactsCard.css";
+import { FaBirthdayCake } from "react-icons/fa";
+
+const ContactsCard = ({ data }) => {
+  return (
+    <div className="ContactsCard">
+      <img src={data.picture} alt="Profile"></img>
+      <div>
+        <div>
+          <span>{data.firstname}&nbsp;</span>
+          <span> {data.lastname}</span>
+        </div>
+        <div>
+          {" "}
+          <FaBirthdayCake size={20} style={{ "margin-right": "5px" }} />{" "}
+          Birthday {new Date(data.birthday).toDateString()}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactsCard;
