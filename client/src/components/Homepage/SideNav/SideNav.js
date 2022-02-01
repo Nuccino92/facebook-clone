c45b1557e5a56322f433a5317b88b317962345d8
Nuccino92 <anthnucci@hotmail.com>
2022-01-31 22:51:45 -0500

    homepade left nav

--- client/src/components/Homepage/SideNav/SideNav.js
+++ client/src/components/Homepage/SideNav/SideNav.js
@@ -1,7 +1,54 @@
 import "./SideNav.css";
+import findFriends from "./SideNavImages/find-friends.png";
+import mostRecent from "./SideNavImages/most-recent.png";
+import liked from "./SideNavImages/liked.png";
+import oldest from "./SideNavImages/oldest.png";
+import sendMessage from "./SideNavImages/send-message.png";
 
+import { useState } from "react";
 const SideNav = () => {
-  return <div className="SideNav">SideNav</div>;
+  const [userData] = useState({
+    firstname: "Anthony",
+    lastname: "Nucci",
+    picture:
+      "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg",
+  });
+
+  return (
+    <div className="SideNav">
+      <ul>
+        <li>
+          <img
+            className="SideNav-profile-img"
+            src={userData.picture}
+            alt="Profile"
+          ></img>
+          <span>{userData.firstname}&nbsp;</span>
+          <span>{userData.lastname}</span>
+        </li>
+        <li>
+          <img src={findFriends} alt="Find friends"></img>
+          <p>Find Friends</p>
+        </li>
+        <li>
+          <img src={mostRecent} alt="Most recent"></img>
+          <p>Most Recent</p>
+        </li>
+        <li>
+          <img src={liked} alt="Liked"></img>
+          <p>Liked</p>
+        </li>
+        <li>
+          <img src={oldest} alt="OLdest"></img>
+          <p>Oldest</p>
+        </li>
+        <li>
+          <img src={sendMessage} alt="Send Message"></img>
+          <p>Send Message</p>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default SideNav;
