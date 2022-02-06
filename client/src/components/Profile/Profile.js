c9c0d871b899fb814372a484d96fc3b58250d53b
Nuccino92 <anthnucci@hotmail.com>
2022-02-05 20:21:00 -0500

    added nested routes

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,13 +1,24 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Nav from "../Nav/Nav";
 import "./Profile.css";
+import ProfileAbout from "./ProfileAbout/ProfileAbout";
+import ProfileFriends from "./ProfileFriends/ProfileFriends";
+import ProfileIndex from "./ProfilePosts/ProfileIndex";
 import ProfileUpper from "./ProfileUpper/ProfileUpper";
 
 const Profile = () => {
+  const [myProfile, setMyProfile] = useState(true);
+
   return (
     <div className="Profile">
-      <Nav />
       <div className="Profile-body">
-        <ProfileUpper />
+        <ProfileUpper myProfile={myProfile} />
+        <Routes>
+          <Route path="/" element={<ProfileIndex myProfile={myProfile} />} />
+          <Route path="about" element={<ProfileAbout />} />
+          <Route path="friends" element={<ProfileFriends />} />
+        </Routes>
       </div>
     </div>
   );
