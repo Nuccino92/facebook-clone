695f9ed2c365a4dfcfb1237a3ffe32e28126fe44
Nuccino92 <anthnucci@hotmail.com>
2022-02-06 23:42:29 -0500

    refactor

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,21 +1,34 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import Nav from "../Nav/Nav";
 import "./Profile.css";
 import ProfileAbout from "./ProfileAbout/ProfileAbout";
 import ProfileFriends from "./ProfileFriends/ProfileFriends";
-import ProfileIndex from "./ProfilePosts/ProfileIndex";
+import ProfileIndex from "./ProfileIndex/ProfileIndex";
 import ProfileUpper from "./ProfileUpper/ProfileUpper";
 
 const Profile = () => {
   const [myProfile, setMyProfile] = useState(true);
 
+  const [selectedTab, setSelectedTab] = useState("posts");
+
   return (
     <div className="Profile">
       <div className="Profile-body">
-        <ProfileUpper myProfile={myProfile} />
+        <ProfileUpper
+          myProfile={myProfile}
+          selectedTab={selectedTab}
+          setSelectedTab={setSelectedTab}
+        />
         <Routes>
-          <Route path="/" element={<ProfileIndex myProfile={myProfile} />} />
+          <Route
+            path="/"
+            element={
+              <ProfileIndex
+                myProfile={myProfile}
+                setSelectedTab={setSelectedTab}
+              />
+            }
+          />
           <Route path="about" element={<ProfileAbout />} />
           <Route path="friends" element={<ProfileFriends />} />
         </Routes>
