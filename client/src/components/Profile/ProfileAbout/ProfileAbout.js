f3bbe26aa4e83df126199fe2981c8a132d48f46c
Nuccino92 <anthnucci@hotmail.com>
2022-02-07 17:55:26 -0500

    cleanup

--- client/src/components/Profile/ProfileAbout/ProfileAbout.js
+++ client/src/components/Profile/ProfileAbout/ProfileAbout.js
@@ -2,6 +2,7 @@ import "./ProfileAbout.css";
 import React from "react";
 import About from "../About/About";
 import Friends from "../Friends/Friends";
+
 const ProfileAbout = () => {
   return (
     <div className="ProfileAbout">
