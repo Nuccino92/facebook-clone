4d1e249a2ff4ad8b47fd255aecdc3fcb50396b78
Nuccino92 <anthnucci@hotmail.com>
2022-02-09 17:03:07 -0500

    fixed functions

--- server/utils/passwords.js
+++ server/utils/passwords.js
@@ -1,11 +1,11 @@
 import bcrypt from "bcryptjs";
 
 export const generateHash = (password) => {
-  const salt = bcrypt.genSalt(12);
+  const salt = bcrypt.genSaltSync(12);
   const hash = bcrypt.hashSync(password, salt);
   return hash;
 };
 
 export const compareHash = (password, hashed) => {
-  return bcrypt.compareHash(password, hashed);
+  return bcrypt.compareSync(password, hashed);
 };
