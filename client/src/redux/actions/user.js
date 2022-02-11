97886bc214c86a04a6a94517b64e57e278bd00d9
Nuccino92 <anthnucci@hotmail.com>
2022-02-10 23:51:15 -0500

    added error handling

--- client/src/redux/actions/user.js
+++ client/src/redux/actions/user.js
@@ -1,4 +1,5 @@
 import { createUserRequest } from "../../api/user";
+import { returnErrors } from "./errors";
 
 import { REGISTER_FAIL, REGISTER_SUCCESS } from "./types";
 
@@ -6,9 +7,18 @@ export const createUser = (userData) => async (dispatch) => {
   try {
     const res = await createUserRequest(userData);
     dispatch({ type: REGISTER_SUCCESS, payload: res.data });
+    dispatch(returnErrors({}, null, null, "REGISTER_SUCCESS"));
   } catch (err) {
     dispatch({
       type: REGISTER_FAIL,
     });
+    dispatch(
+      returnErrors(
+        err.response.data[0].msg,
+        err.response.status,
+        err.response.data[0].param,
+        "REGISTER_FAIL"
+      )
+    );
   }
 };
