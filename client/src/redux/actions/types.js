0cbaeb9adcb7eff8d0544fb0dcdbca74dfecbff1
Nuccino92 <anthnucci@hotmail.com>
2022-02-11 18:20:46 -0500

    added logout, loading, and auth types

--- client/src/redux/actions/types.js
+++ client/src/redux/actions/types.js
@@ -3,6 +3,10 @@ export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAIL = "REGISTER_FAIL";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
+export const USER_LOADING = "USER_LOADING";
+export const USER_LOADED = "USER_LOADED";
+export const AUTH_ERR = "AUTH_ERROR";
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
 // errors
 export const GET_ERRORS = "GET_ERRORS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
