2640e5c42bc6658dab3c83311a86bcfd6d1e2c7a
Nuccino92 <anthnucci@hotmail.com>
2022-02-11 16:39:00 -0500

    added login variables

--- client/src/redux/actions/types.js
+++ client/src/redux/actions/types.js
@@ -1,6 +1,8 @@
 // user
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAIL = "REGISTER_FAIL";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
 // errors
 export const GET_ERRORS = "GET_ERRORS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
