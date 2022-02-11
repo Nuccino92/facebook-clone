05c09f25d470de2fd0e6bc4b65821a9c02efd322
Nuccino92 <anthnucci@hotmail.com>
2022-02-10 23:50:48 -0500

    added validation middleware

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { register_Post } from "../controllers/register.js";
+import registerValidation from "../validation/register.js";
 
 const router = express.Router();
 
-router.post("/", register_Post);
+router.post("/", registerValidation, register_Post);
 
 export default router;
