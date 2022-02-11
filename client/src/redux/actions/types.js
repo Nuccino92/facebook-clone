76a8e2693264401d07fbefaeae951c3fdd09e26f
Nuccino92 <anthnucci@hotmail.com>
2022-02-10 23:51:22 -0500

    added error types

--- client/src/redux/actions/types.js
+++ client/src/redux/actions/types.js
@@ -1,3 +1,6 @@
 // user
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAIL = "REGISTER_FAIL";
+// errors
+export const GET_ERRORS = "GET_ERRORS";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
