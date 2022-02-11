08c351a060602e6cf15d2a12e581105d40e9e6e8
Nuccino92 <anthnucci@hotmail.com>
2022-02-10 23:51:02 -0500

    added error reducer

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
 import userReducer from "./user";
+import errorReducer from "./errors";
 
 export default combineReducers({
   userReducer,
+  errorReducer,
 });
