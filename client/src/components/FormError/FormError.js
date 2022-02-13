6efd52ad7c6208528b58db0c3a2c69bf736608dc
Nuccino92 <anthnucci@hotmail.com>
2022-02-13 17:46:12 -0500

    added styling for adding post

--- client/src/components/FormError/FormError.js
+++ client/src/components/FormError/FormError.js
@@ -4,7 +4,19 @@ const FormError = ({ message, location }) => {
   return (
     <div
       className="FormError"
-      style={location === "register" ? { marginTop: "30px" } : null}
+      style={
+        location === "register"
+          ? { marginTop: "30px" }
+          : location === "ProfileIndex"
+          ? {
+              width: "200px",
+              border: "none",
+              backgroundColor: "white",
+              fontWeight: 600,
+              color: "red",
+            }
+          : null
+      }
     >
       {message}
     </div>
