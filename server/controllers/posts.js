e7354af0c1105600aebf5c44f3868737cad69bb1
Nuccino92 <anthnucci@hotmail.com>
2022-02-13 17:49:53 -0500

    add post controller

---
+++ server/controllers/posts.js
@@ -0,0 +1,25 @@
+import Post from "../models/Post.js";
+import User from "../models/User.js";
+
+export const posts_Post = async (req, res) => {
+  const { user, content } = req.body;
+  const { path } = req.file;
+
+  const newPost = Post({
+    user,
+    content,
+    picture: path,
+  });
+
+  newPost.save().then(async (response) => {
+    await User.findByIdAndUpdate(
+      user,
+      {
+        $addToSet: { posts: response._id },
+      },
+      { new: true }
+    ).then((user) => {
+      return res.status(201).json({ user, newPost });
+    });
+  });
+};
