fa01eb202ee948cad8bf82e5e3fcc790c59fe74a
Nuccino92 <anthnucci@hotmail.com>
2022-02-13 17:45:37 -0500

    split auth routes with non auth

--- client/src/App.js
+++ client/src/App.js
@@ -2,28 +2,47 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LogIn from "./components/LogIn/LogIn";
 import LogInForm from "./components/LogIn/LogInForm";
-import { useState } from "react";
+import { useEffect } from "react";
 import Register from "./components/Register/Register";
 import Homepage from "./components/Homepage/Homepage";
 import Profile from "./components/Profile/Profile";
 import Nav from "./components/Nav/Nav";
 import FriendRequests from "./components/FriendRequests/FriendRequests";
+import { useDispatch, useSelector } from "react-redux";
+import { loadUser } from "./redux/actions/user";
 
 function App() {
-  const [user] = useState(true);
+  const dispatch = useDispatch();
+
+  const { isAuthenticated } = useSelector((state) => state.userReducer);
+
+  useEffect(() => {
+    dispatch(loadUser());
+  }, [dispatch]);
+
   return (
     <BrowserRouter>
       <div className="App">
-        <Nav />
+        {isAuthenticated && <Nav />}
+        {isAuthenticated && <div style={{ marginTop: "56px" }}></div>}
+        {isAuthenticated ? (
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/login" element={<LogInForm />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile/*" element={<Profile />} />
+            <Route path="/friends" element={<FriendRequests />} />
+          </Routes>
+        ) : (
+          <Routes>
+            <Route path="/" element={<LogIn />} />
+            <Route path="/login" element={<LogInForm />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile/*" element={<LogIn />} />
+            <Route path="/friends" element={<LogIn />} />
+          </Routes>
+        )}{" "}
         {/* spaces content below fixed header */}
-        <div style={{ marginTop: "56px" }}></div>
-        <Routes>
-          <Route path="/" element={user ? <Homepage /> : <LogIn />} />
-          <Route path="/login" element={<LogInForm />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile/*" element={<Profile />} />
-          <Route path="/friends" element={<FriendRequests />} />
-        </Routes>
       </div>
     </BrowserRouter>
   );
