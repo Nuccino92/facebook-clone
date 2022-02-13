e7429739a410c3306d09e4e4af88edac9d54f9d6
Nuccino92 <anthnucci@hotmail.com>
2022-02-13 17:47:32 -0500

    post handle submit dispatches to addpost, also error handling

--- client/src/components/Profile/ProfileIndex/ProfileIndex.js
+++ client/src/components/Profile/ProfileIndex/ProfileIndex.js
@@ -1,11 +1,56 @@
 import "./ProfileIndex.css";
 import Post from "../../Post/Post";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FcStackOfPhotos } from "react-icons/fc";
 import { Link } from "react-router-dom";
 import About from "../About/About";
+import { useDispatch, useSelector } from "react-redux";
+import { addPost } from "../../../redux/actions/post";
+import FormError from "../../FormError/FormError";
 
 const ProfileIndex = ({ myProfile, setSelectedTab }) => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.userReducer);
+
+  const [createPostData, setCreatePostData] = useState({
+    content: null,
+    user: user,
+    picture: null,
+  });
+
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value, files, type } = e.target;
+
+    setErrorMessage(null);
+
+    setCreatePostData((prev) => {
+      return {
+        ...prev,
+        [name]: type === "file" ? files[0] : value,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!createPostData.content) {
+      return setErrorMessage("Please add description");
+    } else if (!createPostData.picture) {
+      return setErrorMessage("Please upload an image");
+    } else {
+      const data = new FormData();
+
+      data.append("content", createPostData.content);
+      data.append("user", createPostData.user._id);
+      data.append("picture", createPostData.picture);
+
+      dispatch(addPost(data));
+    }
+  };
+
   const picture =
     "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg";
 
@@ -246,15 +291,25 @@ const ProfileIndex = ({ myProfile, setSelectedTab }) => {
               <img src={picture} alt="Profile"></img>
               <input
                 type="text"
-                name="post"
+                name="content"
                 placeholder="What's on your mind?"
+                onChange={handleChange}
               ></input>
             </div>
-            <div style={{ marginTop: "20px" }}>
+            <div style={{ marginTop: "20px", justifyContent: "space-between" }}>
               <label htmlFor="postPicture" className="custom-file-upload">
                 <FcStackOfPhotos /> Photo
-                <input name="picture" id="postPicture" type="file" />
+                <input
+                  name="picture"
+                  id="postPicture"
+                  type="file"
+                  onChange={handleChange}
+                />
               </label>
+              {errorMessage && (
+                <FormError message={errorMessage} location={"ProfileIndex"} />
+              )}
+              <button onClick={handleSubmit}>Add Post</button>
             </div>
           </div>
         )}
