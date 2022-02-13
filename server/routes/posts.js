4ae21a8d02809084f5ffba80b4ed567a815f7f72
Nuccino92 <anthnucci@hotmail.com>
2022-02-13 17:50:21 -0500

    posts route w/ multer middleware

---
+++ server/routes/posts.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { posts_Post } from "../controllers/posts.js";
+
+const router = express.Router();
+
+import multer from "multer";
+
+// define storage for images
+
+const fileStorageEngine = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads/images");
+  },
+
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: fileStorageEngine,
+  limits: {
+    fileSize: 1024 * 1024 * 10,
+  },
+});
+
+router.post("/", upload.single("picture"), posts_Post);
+
+export default router;
