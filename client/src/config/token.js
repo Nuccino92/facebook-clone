6e3069bf6cf1e3329f96022e7e0a912d2362dd04
Nuccino92 <anthnucci@hotmail.com>
2022-02-13 17:47:53 -0500

    token config, handles jwt

---
+++ client/src/config/token.js
@@ -0,0 +1,25 @@
+// grabs the token and puts it in the header
+export const tokenConfig = (getState) => {
+  // grabbing token from userReducer
+  const token = getState().userReducer.token;
+  const config = {
+    headers: {},
+  };
+  //if token has no headers
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+  return config;
+};
+
+// get token if paged is refreshed
+export const tokenRefreshConfig = (token) => {
+  const config = {
+    headers: {},
+  };
+  //if token has no headers
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+  return config;
+};
