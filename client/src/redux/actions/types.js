db96c9b02c2ae4c498ea044a9ad4add96d8dbc7f
Nuccino92 <anthnucci@hotmail.com>
2022-02-15 02:14:18 -0500

    viewed user types

--- client/src/redux/actions/types.js
+++ client/src/redux/actions/types.js
@@ -10,3 +10,11 @@ export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
 // errors
 export const GET_ERRORS = "GET_ERRORS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
+//posts
+export const CREATE_POST = "CREATE_POST";
+// focusedUser
+export const GET_VIEWED_USER = "GET_VIEWEDEDUSER";
+export const VIEWED_USER_LOADING = "VIEWED_USER_LOADING";
+export const VIEWED_USER_LOADED = "VIEWED_USER_LOADED";
+export const GET_FRIEND_STATUS = "GET_FRIEND_STATUS";
+export const MY_PROFILE = "MY_PROFILE";
