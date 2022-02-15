319e11e85e802e4aa41ce6405063506251f0bda8
Nuccino92 <anthnucci@hotmail.com>
2022-02-14 22:58:45 -0500

    passed state through to components

--- client/src/components/Profile/ProfileIndex/ProfileIndex.js
+++ client/src/components/Profile/ProfileIndex/ProfileIndex.js
@@ -1,20 +1,26 @@
 import "./ProfileIndex.css";
 import Post from "../../Post/Post";
-import { useEffect, useState } from "react";
+import { useRef, useState } from "react";
 import { FcStackOfPhotos } from "react-icons/fc";
 import { Link } from "react-router-dom";
 import About from "../About/About";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { addPost } from "../../../redux/actions/post";
 import FormError from "../../FormError/FormError";
 
-const ProfileIndex = ({ myProfile, setSelectedTab }) => {
+const ProfileIndex = ({
+  myProfile,
+  setSelectedTab,
+  user: focusedUser,
+  friendStatus,
+}) => {
   const dispatch = useDispatch();
-  const { user } = useSelector((state) => state.userReducer);
+
+  const formRef = useRef(null);
 
   const [createPostData, setCreatePostData] = useState({
     content: null,
-    user: user,
+    user: focusedUser,
     picture: null,
   });
 
@@ -48,7 +54,15 @@ const ProfileIndex = ({ myProfile, setSelectedTab }) => {
       data.append("picture", createPostData.picture);
 
       dispatch(addPost(data));
+
+      setCreatePostData({
+        content: null,
+        user: focusedUser,
+        picture: null,
+      });
     }
+
+    formRef.current.reset();
   };
 
   const picture =
@@ -207,70 +221,30 @@ const ProfileIndex = ({ myProfile, setSelectedTab }) => {
     },
   ]);
 
-  const [userData] = useState([
-    {
-      firstname: "Demetrius",
-      email: "mauris@protonmail.org",
-      lastname: "Allen Lambert",
-      picture:
-        "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg",
-      birthday: "Sat Jan 01 2022 00:00:00 GMT-0500 (Eastern Standard Time)",
-    },
-    {
-      firstname: "Cruz",
-      email: "dis.parturient.montes@aol.couk",
-      lastname: "Bertha",
-      picture:
-        "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg",
-      birthday: "Sat Jan 01 2022 00:00:00 GMT-0500 (Eastern Standard Time)",
-    },
-    {
-      firstname: "George",
-      email: "laoreet.ipsum.curabitur@icloud.ca",
-      lastname: "Margaret",
-      picture:
-        "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg",
-      birthday: "Sat Jan 01 2022 00:00:00 GMT-0500 (Eastern Standard Time)",
-    },
-    {
-      firstname: "Warren",
-      email: "accumsan@protonmail.com",
-      lastname: "Pascale",
-      picture:
-        "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg",
-      birthday: "Sat Jan 01 2022 00:00:00 GMT-0500 (Eastern Standard Time)",
-    },
-
-    {
-      firstname: "Gabriel",
-      email: "vulputate@hotmail.couk",
-      lastname: "Hayes",
-      picture:
-        "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg",
-      birthday: "Sat Jan 01 2022 00:00:00 GMT-0500 (Eastern Standard Time)",
-    },
-  ]);
-
   return (
     <div className="ProfileIndex">
       {/* ---------left side index------------ */}
       <div className="ProfileIndex-left">
         {/* -------------------intro portion-----------------------  */}
-        <About />
+        <About
+          user={focusedUser}
+          friendStatus={friendStatus}
+          myProfile={myProfile}
+        />
         {/* -----------------friends portion------------------ */}
         <div className="ProfileIndex-friends">
           <div className="ProfileIndex-friends-header">
             <h2>Friends</h2>
             <Link
-              to={`/profile/friends`}
+              to={`/profile/${focusedUser._id}/friends`}
               onClick={() => setSelectedTab("friends")}
             >
               <span>See all friends</span>
             </Link>
           </div>
-          <p>{userData.length} Friends</p>
+          <p>{focusedUser.friends.length} Friends</p>
           <div className="ProfileIndex-friends-list-container">
-            {userData.map((user, index) => {
+            {focusedUser.friends.map((user, index) => {
               return (
                 <div key={index} className="ProfileIndex-friends-list">
                   <img src={user.picture} alt="Profile"></img>
@@ -286,32 +260,36 @@ const ProfileIndex = ({ myProfile, setSelectedTab }) => {
       {/* -------------right side index ------------------ */}
       <div className="ProfileIndex-right">
         {myProfile && (
-          <div className="ProfileCreatePost-container">
-            <div>
-              <img src={picture} alt="Profile"></img>
-              <input
-                type="text"
-                name="content"
-                placeholder="What's on your mind?"
-                onChange={handleChange}
-              ></input>
-            </div>
-            <div style={{ marginTop: "20px", justifyContent: "space-between" }}>
-              <label htmlFor="postPicture" className="custom-file-upload">
-                <FcStackOfPhotos /> Photo
+          <form ref={formRef}>
+            <div className="ProfileCreatePost-container">
+              <div>
+                <img src={picture} alt="Profile"></img>
                 <input
-                  name="picture"
-                  id="postPicture"
-                  type="file"
+                  type="text"
+                  name="content"
+                  placeholder="What's on your mind?"
                   onChange={handleChange}
-                />
-              </label>
-              {errorMessage && (
-                <FormError message={errorMessage} location={"ProfileIndex"} />
-              )}
-              <button onClick={handleSubmit}>Add Post</button>
-            </div>
-          </div>
+                ></input>
+              </div>
+              <div
+                style={{ marginTop: "20px", justifyContent: "space-between" }}
+              >
+                <label htmlFor="postPicture" className="custom-file-upload">
+                  <FcStackOfPhotos /> Photo
+                  <input
+                    name="picture"
+                    id="postPicture"
+                    type="file"
+                    onChange={handleChange}
+                  />
+                </label>
+                {errorMessage && (
+                  <FormError message={errorMessage} location={"ProfileIndex"} />
+                )}
+                <button onClick={handleSubmit}>Add Post</button>
+              </div>
+            </div>{" "}
+          </form>
         )}
         {/* ------------------posts portion---------------- */}
         <div className="ProfileIndex-post-container">
