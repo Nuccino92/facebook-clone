0ded12fc1a91eae8c51febbec322f9d43d4e01d8
Nuccino92 <anthnucci@hotmail.com>
2022-02-15 20:01:02 -0500

    post request returns new array of data w/ all friends

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -6,11 +6,27 @@ export const user_Get = async (req, res) => {
   await User.findById(id)
     .select("-password")
     .then((response) => {
-      console.log(response);
       res.status(201).json({ response });
     })
     .catch((err) => {
       console.log(err);
       res.status(404).json({ err });
+    });
+};
+
+export const userFriends_Post = async (req, res) => {
+  const friends = req.body;
+
+  // wrapped in promise all in order to use map & return the array of data
+  await Promise.all(
+    friends.map(
+      async (friend) => await User.findById(friend).select("-password")
+    )
+  )
+    .then((response) => {
+      return res.status(201).json(response);
+    })
+    .catch((err) => {
+      return res.status(400).json({ err });
     });
 };
