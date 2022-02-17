c9323c78014eacb9721b2172f0e1291873dc4e24
Nuccino92 <anthnucci@hotmail.com>
2022-02-16 23:38:03 -0500

    refactor

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-export const getUserRequest = async (id) =>
-  await axios.get(`http://localhost:8000/user/${id}`);
+const url = "http://localhost:8000/user/";
+
+export const getUserRequest = async (id) => await axios.get(url + id);
 
 export const getUserFriendsRequest = (id, friends) =>
-  axios.post(`http://localhost:8000/user/friends/${id}`, friends);
+  axios.post(url + `friends/${id}`, friends);
