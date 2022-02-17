d3073c55db0991c229921229e03f2323324117c9
Nuccino92 <anthnucci@hotmail.com>
2022-02-16 23:37:55 -0500

    get posts of user

--- client/src/api/post.js
+++ client/src/api/post.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const url = "http://localhost:8000/posts";
+const url = "http://localhost:8000/posts/";
 
 export const addPostRequest = (postData) => axios.post(url, postData);
+
+export const getUserPostsRequest = (id, posts) => axios.post(url + id, posts);
