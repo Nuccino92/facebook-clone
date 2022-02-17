6bf36ba47455ab058b48118249a800d4b70e0404
Nuccino92 <anthnucci@hotmail.com>
2022-02-16 23:40:14 -0500

    refactored to use actual data

--- client/src/components/Profile/ProfileIndex/ProfileIndex.js
+++ client/src/components/Profile/ProfileIndex/ProfileIndex.js
@@ -7,13 +7,14 @@ import About from "../About/About";
 import { useDispatch, useSelector } from "react-redux";
 import { addPost } from "../../../redux/actions/post";
 import FormError from "../../FormError/FormError";
+import whereGif from "./where.gif";
 
 const ProfileIndex = ({ setSelectedTab }) => {
   const dispatch = useDispatch();
 
   const formRef = useRef(null);
 
-  const { myProfile, viewedUser } = useSelector(
+  const { myProfile, viewedUser, friendsInfo, postsInfo } = useSelector(
     (state) => state.viewedUserReducer
   );
 
@@ -67,159 +68,6 @@ const ProfileIndex = ({ setSelectedTab }) => {
   const picture =
     "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg";
 
-  const [post, usePost] = useState([
-    {
-      user: "Anthony Nucci",
-      userPicture:
-        "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg",
-      createdAt: "Sat Jan 5 2022 00:00:00 GMT-0500 (Eastern Standard Time)",
-      picture:
-        "https://cochonnailles.ca/wp-content/uploads/2013/12/random-owl.jpg",
-      description: "this is an owl with a sick hat",
-      likes: ["Anthony", "Cruz", "George"],
-      comments: [
-        {
-          user: "Anthony",
-          userPicture:
-            "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg",
-          comment:
-            "I Love this photo I Love this photo I Love this photo I Love this photo I Love this photo",
-          createdAt:
-            "Sat Jan 06 2022 00:00:00 GMT-0500 (Eastern Standard Time)",
-          likes: ["Cruz"],
-          replies: [
-            {
-              user: "Cruz",
-              userPicture:
-                "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg",
-              comment: "Me too!",
-            },
-          ],
-        },
-        {
-          user: "Cruz",
-          userPicture:
-            "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg",
-          comment: "HAHA funny",
-          createdAt:
-            "Sat Jan 08 2022 00:00:00 GMT-0500 (Eastern Standard Time)",
-          likes: ["Anthony"],
-          replies: [],
-        },
-        {
-          user: "George",
-          userPicture:
-            "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg",
-          comment: "bad picture",
-          createdAt:
-            "Sat Jan 11 2022 00:00:00 GMT-0500 (Eastern Standard Time)",
-          likes: [],
-          replies: [],
-        },
-      ],
-    },
-    {
-      user: "Anthony Nucci",
-      userPicture:
-        "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg",
-      createdAt: "Sat Jan 5 2022 00:00:00 GMT-0500 (Eastern Standard Time)",
-      picture:
-        "https://cochonnailles.ca/wp-content/uploads/2013/12/random-owl.jpg",
-      description: "this is an owl with a sick hat",
-      likes: ["Anthony", "Cruz", "George"],
-      comments: [
-        {
-          user: "Anthony",
-          userPicture:
-            "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg",
-          comment:
-            "I Love this photo I Love this photo I Love this photo I Love this photo I Love this photo",
-          createdAt:
-            "Sat Jan 06 2022 00:00:00 GMT-0500 (Eastern Standard Time)",
-          likes: ["Cruz"],
-          replies: [
-            {
-              user: "Cruz",
-              userPicture:
-                "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg",
-              comment: "Me too!",
-            },
-          ],
-        },
-        {
-          user: "Cruz",
-          userPicture:
-            "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg",
-          comment: "HAHA funny",
-          createdAt:
-            "Sat Jan 08 2022 00:00:00 GMT-0500 (Eastern Standard Time)",
-          likes: ["Anthony"],
-          replies: [],
-        },
-        {
-          user: "George",
-          userPicture:
-            "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg",
-          comment: "bad picture",
-          createdAt:
-            "Sat Jan 11 2022 00:00:00 GMT-0500 (Eastern Standard Time)",
-          likes: [],
-          replies: [],
-        },
-      ],
-    },
-    {
-      user: "Anthony Nucci",
-      userPicture:
-        "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg",
-      createdAt: "Sat Jan 5 2022 00:00:00 GMT-0500 (Eastern Standard Time)",
-      picture:
-        "https://cochonnailles.ca/wp-content/uploads/2013/12/random-owl.jpg",
-      description: "this is an owl with a sick hat",
-      likes: ["Anthony", "Cruz", "George"],
-      comments: [
-        {
-          user: "Anthony",
-          userPicture:
-            "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg",
-          comment:
-            "I Love this photo I Love this photo I Love this photo I Love this photo I Love this photo",
-          createdAt:
-            "Sat Jan 06 2022 00:00:00 GMT-0500 (Eastern Standard Time)",
-          likes: ["Cruz"],
-          replies: [
-            {
-              user: "Cruz",
-              userPicture:
-                "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg",
-              comment: "Me too!",
-            },
-          ],
-        },
-        {
-          user: "Cruz",
-          userPicture:
-            "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg",
-          comment: "HAHA funny",
-          createdAt:
-            "Sat Jan 08 2022 00:00:00 GMT-0500 (Eastern Standard Time)",
-          likes: ["Anthony"],
-          replies: [],
-        },
-        {
-          user: "George",
-          userPicture:
-            "https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg",
-          comment: "bad picture",
-          createdAt:
-            "Sat Jan 11 2022 00:00:00 GMT-0500 (Eastern Standard Time)",
-          likes: [],
-          replies: [],
-        },
-      ],
-    },
-  ]);
-
   return (
     <div className="ProfileIndex">
       {/* ---------left side index------------ */}
@@ -237,15 +85,22 @@ const ProfileIndex = ({ setSelectedTab }) => {
               <span>See all friends</span>
             </Link>
           </div>
-          <p>{viewedUser.friends.length} Friends</p>
+          <p>
+            {viewedUser.friends.length}{" "}
+            {viewedUser.friends.length === 1 ? "Friend" : "Friends"}
+          </p>
           <div className="ProfileIndex-friends-list-container">
-            {viewedUser.friends.map((user, index) => {
+            {friendsInfo.map((user, index) => {
+              const { profile } = user;
               return (
-                <div key={index} className="ProfileIndex-friends-list">
-                  <img src={user.picture} alt="Profile"></img>
-                  <span>{user.firstname}</span>
-                  <span>{user.lastname}</span>
-                </div>
+                <Link to={`/profile/${user._id}`} key={index}>
+                  <div className="ProfileIndex-friends-list">
+                    <img src={profile[0].profilePicture} alt="Profile"></img>
+                    <span>
+                      {profile[0].firstName} {profile[0].lastName}
+                    </span>
+                  </div>{" "}
+                </Link>
               );
             })}
           </div>
@@ -287,11 +142,18 @@ const ProfileIndex = ({ setSelectedTab }) => {
           </form>
         )}
         {/* ------------------posts portion---------------- */}
-        <div className="ProfileIndex-post-container">
-          {post.map((p, index) => {
-            return <Post p={p} key={index} />;
-          })}
-        </div>
+        {viewedUser.posts.length === 0 ? (
+          <div className="ProfileIndex-post-no-posts">
+            <h1>{viewedUser.profile[0].firstName} hasn't posted anything :(</h1>
+            <img src={whereGif} alt="where are the posts"></img>
+          </div>
+        ) : (
+          <div className="ProfileIndex-post-container">
+            {postsInfo.map((p, index) => {
+              return <Post post={p} key={index} />;
+            })}
+          </div>
+        )}
       </div>
     </div>
   );
