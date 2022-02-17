5994c0cf3cab70f4a8fab92b2ecae6332ac28fa6
Nuccino92 <anthnucci@hotmail.com>
2022-02-16 23:37:39 -0500

    api calls for comments

---
+++ client/src/api/comment.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const url = "http://localhost:8000/comment/";
+
+export const addCommentRequest = (data) => axios.post(url, data);
+export const getCommentRequest = (id) => axios.get(url + id);
+export const getCommentAuthorRequest = (id) => axios.get(url + "user" + id);
