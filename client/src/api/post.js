515370c75e7643e8a94c0c7080e2d91a4a2d76f7
Nuccino92 <anthnucci@hotmail.com>
2022-02-18 16:53:13 -0500

    like/unlike api call

--- client/src/api/post.js
+++ client/src/api/post.js
@@ -5,3 +5,6 @@ const url = "http://localhost:8000/posts/";
 export const addPostRequest = (postData) => axios.post(url, postData);
 
 export const getUserPostsRequest = (id, posts) => axios.post(url + id, posts);
+
+export const updatePostLikesRequest = (id, post) =>
+  axios.post(url + `like/${id}`, post);
