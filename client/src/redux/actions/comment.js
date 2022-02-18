d5145e41da0593909c00839eb50cd0d8411860b5
Nuccino92 <anthnucci@hotmail.com>
2022-02-17 20:47:15 -0500

    deleted comment actions

--- client/src/redux/actions/comment.js
+++
@@ -1,9 +0,0 @@
-import { addCommentRequest } from "../../api/comment";
-
-export const addComment = (data) => async (dispatch) => {
-  try {
-    await addCommentRequest(data);
-  } catch (err) {
-    console.log(err);
-  }
-};
