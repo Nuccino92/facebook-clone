134be30529169eb576931abbf9647373681d1ca4
Nuccino92 <anthnucci@hotmail.com>
2022-02-17 20:47:30 -0500

    completed reply section

--- client/src/components/Post/CommentsSection/Comment/Reply/Reply.js
+++ client/src/components/Post/CommentsSection/Comment/Reply/Reply.js
@@ -1,15 +1,47 @@
+import { useEffect, useState } from "react";
+import moment from "moment";
+import { Link } from "react-router-dom";
+import { getUserRequest } from "../../../../../api/user";
 import "./Reply.css";
 
-const Reply = ({ reply, username }) => {
-  const { user, userPicture, comment } = reply;
-  return (
+const Reply = ({ reply, userData }) => {
+  // user data of author of this reply
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    if (reply !== null) {
+      const getUserData = async () => {
+        const res = await getUserRequest(reply.user);
+        setUser(res.data.response);
+      };
+      getUserData();
+    }
+  }, [reply]);
+
+  return user === null ? (
+    <></>
+  ) : (
     <div className="Reply">
-      <img src={userPicture} alt="Profile"></img>
+      {" "}
+      <Link to={`/profile/${user._id}`}>
+        <img src={userData.profilePicture} alt="Profile"></img>
+      </Link>
       <div>
-        <h4>{user}</h4>
-        <p>
-          <span>{username}</span> {comment}
-        </p>
+        <div>
+          {" "}
+          <Link to={`/profile/${user._id}`}>
+            <h4>
+              {user.profile[0].firstName} {user.profile[0].lastName}
+            </h4>
+          </Link>
+          <p>
+            <span>
+              {userData.firstName} {userData.lastName}
+            </span>{" "}
+            {reply.content}
+          </p>
+        </div>{" "}
+        <p>{moment(new Date(reply.createdAt)).fromNow()}</p>
       </div>
     </div>
   );
