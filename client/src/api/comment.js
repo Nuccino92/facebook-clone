f94028a2ea03c02f85398b4e4e385e02cac39cd0
Nuccino92 <anthnucci@hotmail.com>
2022-02-17 20:50:07 -0500

    request to add reply

--- client/src/api/comment.js
+++ client/src/api/comment.js
@@ -3,5 +3,7 @@ import axios from "axios";
 const url = "http://localhost:8000/comment/";
 
 export const addCommentRequest = (data) => axios.post(url, data);
+export const addCommentReplyRequest = (data) => axios.post(url + "reply", data);
+
 export const getCommentRequest = (id) => axios.get(url + id);
 export const getCommentAuthorRequest = (id) => axios.get(url + "user" + id);
