3e186edc5b2704823cc1ac51ad85e81141ba2b7c
Nuccino92 <anthnucci@hotmail.com>
2022-02-19 23:28:22 -0500

    refactored code to fit multiple sets of data

--- client/src/api/post.js
+++ client/src/api/post.js
@@ -6,5 +6,12 @@ export const addPostRequest = (postData) => axios.post(url, postData);
 
 export const getUserPostsRequest = (id, posts) => axios.post(url + id, posts);
 
-export const updatePostLikesRequest = (id, post) =>
-  axios.post(url + `like/${id}`, post);
+export const updatePostLikesRequest = (id, post, user) =>
+  axios({
+    method: "post",
+    url: url + `like/${id}`,
+    data: {
+      post,
+      userId: user._id,
+    },
+  });
