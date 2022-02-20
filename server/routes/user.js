dd2540c7800489ea3e5d43bbab84713cd8b4ed92
Nuccino92 <anthnucci@hotmail.com>
2022-02-19 23:25:41 -0500

    storing posts the user has liked route

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { user_Get, userFriends_Post } from "../controllers/user.js";
+import {
+  user_Get,
+  userFriends_Post,
+  userTimeline_Get,
+  userAddLikedPosts_Post,
+} from "../controllers/user.js";
 
 const router = express.Router();
 
 router.get("/:id", user_Get);
+router.post("/timeline/:id", userTimeline_Get);
 router.post("/friends/:id", userFriends_Post);
+router.post("/liked/:id", userAddLikedPosts_Post);
 
 export default router;
