318e60fa4f0b4b81240cbe0636b3d8293421a211
Nuccino92 <anthnucci@hotmail.com>
2022-02-19 23:30:37 -0500

    add to liked posts/get timeline api requests

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -6,3 +6,9 @@ export const getUserRequest = async (id) => await axios.get(url + id);
 
 export const getUserFriendsRequest = (id, friends) =>
   axios.post(url + `friends/${id}`, friends);
+
+export const getUserTimelineRequest = (id, friends) =>
+  axios.post(url + `timeline/${id}`, friends);
+
+export const addLikedPostRequest = (id, post) =>
+  axios.post(url + `liked/${id}`, post);
