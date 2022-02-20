f23989b8289a079f614d0cd8bb7e37fa37454488
Nuccino92 <anthnucci@hotmail.com>
2022-02-19 23:30:10 -0500

    retrieves timeline from reducer on mount

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -1,10 +1,21 @@
-import Nav from "../Nav/Nav";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserTimeline, updateTimeline } from "../../redux/actions/user";
 import Contacts from "./Contacts/Contacts";
 import "./Homepage.css";
 import SideNav from "./SideNav/SideNav";
 import Timeline from "./Timeline/Timeline";
 
 const Homepage = () => {
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.userReducer);
+
+  useEffect(() => {
+    if (user.posts.length === 0) return;
+    dispatch(getUserTimeline(user._id, user.friends));
+  }, [dispatch, user._id, user.friends, user.posts.length]);
+
   return (
     <div className="Homepage">
       <div className="Homepage-body">
