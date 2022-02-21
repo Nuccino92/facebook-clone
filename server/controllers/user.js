165ec1f2d2b5e327910787108a621f02ba1de8b7
Nuccino92 <anthnucci@hotmail.com>
2022-02-20 20:07:45 -0500

    add/remove likes & get user timeline

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,4 @@
+import Post from "../models/Post.js";
 import User from "../models/User.js";
 
 export const user_Get = async (req, res) => {
@@ -11,6 +12,33 @@ export const user_Get = async (req, res) => {
     .catch((err) => {
       console.log(err);
       res.status(404).json({ err });
+    });
+};
+
+export const userTimeline_Get = async (req, res) => {
+  const { id } = req.params;
+  const array = [...req.body];
+  let postsArray = [];
+
+  array.push(id);
+
+  await Promise.all(
+    array.map(async (friend) => await User.findById(friend))
+  ).then((response) => {
+    const array = [];
+    response.map((post) => {
+      post.posts.forEach((p) => {
+        postsArray.push(p);
+      });
+    });
+  });
+
+  await Promise.all(postsArray.map(async (post) => await Post.findById(post)))
+    .then((response) => {
+      return res.status(201).json(response);
+    })
+    .catch((err) => {
+      return res.status(409).json(err);
     });
 };
 
@@ -29,4 +57,44 @@ export const userFriends_Post = async (req, res) => {
     .catch((err) => {
       return res.status(400).json({ err });
     });
+};
+
+export const userAddLikedPosts_Post = async (req, res) => {
+  const { id } = req.params;
+  const { _id: post } = req.body.post;
+  const { likedPosts } = req.body.user;
+
+  const alreadyLiked = likedPosts.includes(post);
+
+  //if post id is already in likedPosts array pull it, if not push it
+
+  if (alreadyLiked) {
+    await User.findByIdAndUpdate(
+      id,
+      {
+        $pull: { likedPosts: post },
+      },
+      { new: true }
+    )
+      .then((user) => {
+        return res.status(201).json(user);
+      })
+      .catch((err) => {
+        return res.status(409).json(err);
+      });
+  } else {
+    await User.findByIdAndUpdate(
+      id,
+      {
+        $addToSet: { likedPosts: post },
+      },
+      { new: true }
+    )
+      .then((user) => {
+        return res.status(201).json(user);
+      })
+      .catch((err) => {
+        return res.status(409).json(err);
+      });
+  }
 };
