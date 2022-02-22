5853281afa91a3b991ab2970dda9ff9b4c5119d0
Nuccino92 <anthnucci@hotmail.com>
2022-02-21 21:24:28 -0500

    implemented a usable search bar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,23 +1,63 @@
 import "./Nav.css";
 import facebookIcon from "./facebook-icon.png";
 import { AiOutlineSearch, AiFillCaretDown } from "react-icons/ai";
+import { MdOutlineKeyboardBackspace } from "react-icons/md";
 import { IoMdHome } from "react-icons/io";
 import { BsPersonSquare } from "react-icons/bs";
 import { FaUserFriends } from "react-icons/fa";
 import { GoSignOut } from "react-icons/go";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { logOutUser } from "../../redux/actions/user";
+import { getAllUsers } from "../../api/user";
+import SearchResult from "./SearchResult/SearchResult";
 
 const Nav = () => {
   const dispatch = useDispatch();
   const location = useLocation();
   const navigate = useNavigate();
+  const inputRef = useRef();
+
+  const [profileMenu, setProfileMenu] = useState(false);
+  const [listOfUsers, setListOfUsers] = useState([]);
+  const [filteredData, setFitleredData] = useState([]);
+
+  const [isMyInputFocused, setIsMyInputFocused] = useState(false);
 
   const { user } = useSelector((state) => state.userReducer);
 
-  const [profileMenu, setProfileMenu] = useState(false);
+  useEffect(() => {
+    const getList = async () => {
+      const res = await getAllUsers();
+      setListOfUsers(res.data);
+    };
+    getList();
+  }, []);
+
+  const handleFilter = (e) => {
+    const word = e.target.value;
+
+    const newFilter = listOfUsers.filter((user) => {
+      return (
+        user.profile[0].firstName.toLowerCase().includes(word.toLowerCase()) ||
+        user.profile[0].lastName.toLowerCase().includes(word.toLowerCase())
+      );
+    });
+
+    word === "" ? setFitleredData([]) : setFitleredData(newFilter);
+  };
+
+  // set timeout before setIsMyInputFocused(false) lets user click links before VDOM is removed
+  const handleBlur = () => {
+    setTimeout(() => {
+      setIsMyInputFocused(false);
+    }, 100);
+  };
+
+  useEffect(() => {
+    isMyInputFocused && inputRef.current.focus();
+  }, [isMyInputFocused]);
 
   const handleMenu = () => {
     setProfileMenu(!profileMenu);
@@ -25,16 +65,76 @@ const Nav = () => {
 
   return (
     <div className="Nav">
-      <div className="nav-left">
-        <Link to="/">
-          <img src={facebookIcon} alt="Facebook icon homepage button"></img>
-        </Link>
-
-        <div className="searchbar-container">
-          <AiOutlineSearch />
-          <input placeholder="Search Facebook" type="text"></input>
+      {isMyInputFocused ? (
+        <div className="nav-left nav-left-active">
+          <div>
+            <div
+              style={{ display: "flex", margin: "auto", paddingBottom: "7px" }}
+            >
+              <div className="nav-left-backarrow-container">
+                <MdOutlineKeyboardBackspace size={24} color="grey" />
+              </div>
+              <div className="searchbar-container">
+                <AiOutlineSearch />
+                <input
+                  ref={inputRef}
+                  onBlur={handleBlur}
+                  onFocus={() => setIsMyInputFocused(true)}
+                  placeholder="Search Facebook"
+                  type="text"
+                  onChange={handleFilter}
+                ></input>
+              </div>
+            </div>
+            {isMyInputFocused && (
+              <div className="SearchResult-container">
+                {filteredData.length === 0 ? (
+                  <div className="filtered-data-empty"> No results found</div>
+                ) : (
+                  filteredData.slice(0, 15).map((user, index) => {
+                    return (
+                      <Link to={`/profile/${user._id}`} key={index}>
+                        <SearchResult user={user} />
+                      </Link>
+                    );
+                  })
+                )}
+              </div>
+            )}{" "}
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="nav-left">
+          <Link to="/">
+            <img src={facebookIcon} alt="Facebook icon homepage button"></img>
+          </Link>
+
+          <div className="searchbar-container">
+            <AiOutlineSearch />
+            <input
+              ref={inputRef}
+              onBlur={handleBlur}
+              onFocus={() => setIsMyInputFocused(true)}
+              placeholder="Search Facebook"
+              type="text"
+              onChange={handleFilter}
+            ></input>
+
+            {isMyInputFocused && (
+              <div className="SearchResult-container">
+                {filteredData.slice(0, 15).map((user, index) => {
+                  return (
+                    <Link to={`/profile/${user._id}`} key={index}>
+                      <SearchResult user={user} />
+                    </Link>
+                  );
+                })}
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+
       <div className="nav-middle">
         <Link to="/">
           <div
