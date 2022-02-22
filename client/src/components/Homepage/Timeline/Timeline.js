99986c7b400d6ecce078aa4a06197c0661901594
Nuccino92 <anthnucci@hotmail.com>
2022-02-21 21:25:42 -0500

    jsx to display if nothing on timeline

--- client/src/components/Homepage/Timeline/Timeline.js
+++ client/src/components/Homepage/Timeline/Timeline.js
@@ -1,12 +1,26 @@
 import "./Timeline.css";
 import Post from "../../Post/Post";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Timeline = () => {
-  const { timeline } = useSelector((state) => state.userReducer);
+  const { user, timeline } = useSelector((state) => state.userReducer);
 
   return (
     <div className="Timeline">
+      {timeline.length === 0 && (
+        <div className="Timeline-empty">
+          <div>
+            <p>
+              You have nothing in your timeline. You may like posts, add friends
+              or go to your profile and create one in order to view them here.
+            </p>
+            <Link to={`/profile/${user._id}`}>
+              <button>Profile</button>
+            </Link>
+          </div>
+        </div>
+      )}
       {timeline.map((post, index) => {
         return <Post post={post} key={index} />;
       })}
