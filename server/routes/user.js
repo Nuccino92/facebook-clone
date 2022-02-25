22b0c640afe59e49ffab2f60ab545a56b700e81b
Nuccino92 <anthnucci@hotmail.com>
2022-02-24 20:45:32 -0500

    added file upload middleware

--- server/routes/user.js
+++ server/routes/user.js
@@ -9,9 +9,33 @@ import {
   rejectFriendRequest_Post,
   acceptFriendRequest_Post,
   removeFriend_Post,
+  updateUser_Post,
 } from "../controllers/user.js";
 
 const router = express.Router();
+
+import multer from "multer";
+
+const filesStorageEngine = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: filesStorageEngine,
+  limits: {
+    fileSize: 1024 * 1024 * 10,
+  },
+});
+
+let multipleUpload = upload.fields([
+  { name: "profilePicture" },
+  { name: "coverPhoto" },
+]);
 
 router.get("/:id", user_Get);
 router.get("/", getAllUsers_Get);
@@ -22,5 +46,6 @@ router.post("/send-friend-request/:id", userSendFriendRequest_Post);
 router.post("/reject-friend-request/:id", rejectFriendRequest_Post);
 router.post("/accept-friend-request/:id", acceptFriendRequest_Post);
 router.post("/remove-friend/:id", removeFriend_Post);
+router.put("/update/:id", multipleUpload, updateUser_Post);
 
 export default router;
