feb5ded5b4918c3657383a78b647929655ec13bf
Nuccino92 <anthnucci@hotmail.com>
2022-02-26 00:36:21 -0500

    added file input with handlers

--- client/src/components/Profile/ProfileUpper/EditProfile/EditProfile.js
+++ client/src/components/Profile/ProfileUpper/EditProfile/EditProfile.js
@@ -1,18 +1,25 @@
 import "./EditProfile.css";
 import "./OptionsModals/OptionsModals.css";
 import { AiOutlineClose } from "react-icons/ai";
-import { useSelector } from "react-redux";
-import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
 import ProfilePictureModal from "./OptionsModals/ProfilePictureModal";
 import CoverPhotoModal from "./OptionsModals/CoverPhotoModal";
 import BioModal from "./OptionsModals/BioModal";
+import { updateUser } from "../../../../api/user";
+import convertBase64 from "../../../../utils/convertBase64";
+import { loadUser } from "../../../../redux/actions/user";
 
 const EditProfile = ({ setEditModal }) => {
+  const dispatch = useDispatch();
   const { user } = useSelector((state) => state.userReducer);
 
   const handleModal = (e) => {
     e.target.className === "EditProfile" && setEditModal(false);
   };
+
+  const [uploadProfile, setUploadProfile] = useState(undefined);
+  const [uploadCover, setUploadCover] = useState(undefined);
 
   const [newProfileData, setNewProfileData] = useState({
     profilePicture: user.profile[0].profilePicture,
@@ -24,9 +31,41 @@ const EditProfile = ({ setEditModal }) => {
   const [coverPhotoModal, setCoverPhotoModal] = useState(false);
   const [bioModal, setBioModal] = useState(false);
 
-  useEffect(() => {
-    console.log(newProfileData);
-  }, [newProfileData]);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = new FormData();
+
+    data.append("bio", newProfileData.bio);
+    data.append("profilePicture", newProfileData.profilePicture);
+    data.append("coverPhoto", newProfileData.coverPhoto);
+
+    await updateUser(user._id, data).then(() => {
+      dispatch(loadUser());
+    });
+    setEditModal(false);
+  };
+
+  const handleChange = async (e) => {
+    const { name, value, files, type } = e.target;
+
+    if (type === "file" && name === "profilePicture") {
+      const base64 = await convertBase64(files[0]);
+      setUploadProfile(base64);
+    }
+
+    if (type === "file" && name === "coverPhoto") {
+      const base64 = await convertBase64(files[0]);
+      setUploadCover(base64);
+    }
+
+    setNewProfileData((prev) => {
+      return {
+        ...prev,
+        [name]: type === "file" ? files[0] : value,
+      };
+    });
+  };
 
   return (
     <div className="EditProfile" onClick={handleModal}>
@@ -45,7 +84,15 @@ const EditProfile = ({ setEditModal }) => {
             <h3>Profile Picture</h3>{" "}
             <button onClick={() => setProfilePictureModal(true)}>Add</button>
           </header>
-          <img src={user.profile[0].profilePicture} alt="Profile"></img>
+
+          <img
+            src={
+              uploadProfile === undefined
+                ? user.profile[0].profilePicture
+                : uploadProfile
+            }
+            alt="Profile"
+          />
         </div>
         <div className="EditProfile-cover-photo">
           <header>
@@ -53,12 +100,16 @@ const EditProfile = ({ setEditModal }) => {
             <button onClick={() => setCoverPhotoModal(true)}>Add</button>
           </header>
           <div>
-            {user.profile[0].coverPhoto ? (
+            {user.profile[0].coverPhoto || uploadCover !== undefined ? (
               <img
                 className="EditProfile-cover-photo-image"
-                src={user.profile[0].coverPhoto}
+                src={
+                  uploadCover === undefined
+                    ? user.profile[0].coverPhoto
+                    : uploadCover
+                }
                 alt="cover"
-              ></img>
+              />
             ) : (
               <div className="EditProfile-cover-photo-image  cover-photo-placeholder"></div>
             )}{" "}
@@ -69,22 +120,34 @@ const EditProfile = ({ setEditModal }) => {
             <h3>Bio</h3> <button onClick={() => setBioModal(true)}>Edit</button>
           </header>
           <p>
-            {user.profile[0].bio.length === 0
+            {newProfileData.bio.length === 0
               ? "Nothing in bio"
-              : user.profile[0].bio}
+              : newProfileData.bio}
           </p>
         </div>
         <div className="EditProfile-save">
-          <button>Save Changes</button>
+          <button onClick={handleSubmit}>Save Changes</button>
         </div>
       </div>
       {profilePictureModal && (
-        <ProfilePictureModal setProfilePictureModal={setProfilePictureModal} />
+        <ProfilePictureModal
+          setProfilePictureModal={setProfilePictureModal}
+          handleChange={handleChange}
+        />
       )}
       {coverPhotoModal && (
-        <CoverPhotoModal setCoverPhotoModal={setCoverPhotoModal} />
+        <CoverPhotoModal
+          setCoverPhotoModal={setCoverPhotoModal}
+          handleChange={handleChange}
+        />
       )}
-      {bioModal && <BioModal setBioModal={setBioModal} />}
+      {bioModal && (
+        <BioModal
+          setBioModal={setBioModal}
+          setNewProfileData={setNewProfileData}
+          newProfileData={newProfileData}
+        />
+      )}
     </div>
   );
 };
