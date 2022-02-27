ed70eb9fcbfb94fbf7b9eba466bb612cc7aea0b7
Nuccino92 <anthnucci@hotmail.com>
2022-02-27 17:47:08 -0500

    added conversation reducer

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from "redux";
 import userReducer from "./user";
 import errorReducer from "./errors";
 import viewedUserReducer from "./viewedUser";
+import conversationReducer from "./conversation";
 
 export default combineReducers({
   userReducer,
   errorReducer,
   viewedUserReducer,
+  conversationReducer,
 });
