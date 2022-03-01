8bd7c67d261b868c20917c0a964a57d8b745c65f
Nuccino92 <anthnucci@hotmail.com>
2022-03-01 17:53:19 -0500

    added socket io in server

--- server/app.js
+++ server/app.js
@@ -15,25 +15,69 @@ import commentsRoutes from "./routes/comments.js";
 import messageRoutes from "./routes/messages.js";
 import conversationRoutes from "./routes/conversations.js";
 
-const app = express();
+import { Server } from "socket.io";
+
+export const app = express();
 
 dotenv.config();
 const dbURI = process.env.MONGOOSE_URI;
 
 app.use(passport.initialize());
 app.use("/uploads", express.static("uploads"));
-//handles body requests
+
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cors());
 
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
-    app.listen(8000, () => console.log("Server is running on port 8000"))
+    app.listen(process.env.PORT, () => console.log("Server is running"))
   )
+  .then((server) => {
+    const io = new Server(server, {
+      cors: {
+        origin: "http://localhost:3000" || "facebook-mock-project.netlify.app",
+      },
+    });
+
+    let users = [];
+
+    const addUser = (userId, socketId) => {
+      !users.some((user) => user.userId === userId) &&
+        users.push({ userId, socketId });
+    };
+
+    const getUser = (userId) => {
+      return users.find((user) => user.userId === userId);
+    };
+
+    const removeUser = (socketId) => {
+      users = users.filter((user) => user.socketId !== socketId);
+    };
+
+    io.on("connection", (socket) => {
+      socket.on("addUser", (userId) => {
+        addUser(userId, socket.id);
+        io.emit("getUsers", users);
+      });
+
+      socket.on("sendMessage", ({ senderId, receiverId, content }) => {
+        const user = getUser(receiverId);
+        if (user !== undefined) {
+          io.to(user.socketId).emit("getMessage", {
+            senderId,
+            content,
+          });
+        }
+      });
+
+      socket.on("logOut", () => {
+        removeUser(socket.Id);
+        io.emit("getUsers", users);
+      });
+    });
+  })
   .catch((err) => console.log(err.message));
 
 const db = mongoose.connection;
